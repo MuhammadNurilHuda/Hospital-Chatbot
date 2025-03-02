adcae9d7a81708c1c21d9c0e9eaef4a5ad9303e5
MuhammadNurilHuda <nurilhuda3333@gmail.com>
2025-03-02 15:58:51 +0700

    update class appointment

--- app/database.py
+++ app/database.py
@@ -1,21 +1,22 @@
+# app\database.py
+
 import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine, Column, Integer, String, Date
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 load_dotenv()
 
 DB_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DB_URL)
+engine = create_engine(DB_URL, echo=True)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
 
 class Appointment(Base):
     __tablename__ = "appointments"
 
-    id = Column(Integer, primery_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True)
     name = Column(String(100))
     phone = Column(String(15))
     doctor_specialist = Column(String(100))

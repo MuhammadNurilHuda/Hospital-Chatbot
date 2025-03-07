9a785e382da69798ad066f60e448159d2e90383c
MuhammadNurilHuda <nurilhuda3333@gmail.com>
2025-03-07 15:55:22 +0700

    fix logging issues

--- app/database.py
+++ app/database.py
@@ -2,12 +2,18 @@
 
 import os
 from dotenv import load_dotenv
-from sqlalchemy import create_engine, Column, Integer, String, Date
+from sqlalchemy import (
+    create_engine, Column, 
+    Integer, String, Date, 
+    Text, DateTime)
 from sqlalchemy.orm import sessionmaker, declarative_base
+from datetime import datetime, timezone
+from app.logging import logger
 
 load_dotenv()
 
 DB_URL = os.getenv("DATABASE_URL")
+logger.info(f"Menghubungkan ke database dengan url: {DB_URL}")
 
 engine = create_engine(DB_URL, echo=True)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
@@ -22,4 +28,17 @@ class Appointment(Base):
     doctor_specialist = Column(String(100))
     appointment_date = Column(Date)
 
-Base.metadata.create_all(bind=engine)
+class UserActivityLog(Base):
+    __tablename__ = "user_activity_logs"
+
+    id = Column(Integer, primary_key=True, index=True)
+    prompt = Column(Text, nullable=False)
+    response = Column(Text, nullable=False)
+    timestamp = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
+
+try:
+    Base.metadata.create_all(bind=engine)
+    logger.info("Berhasil membuat tabel database")
+
+except Exception as e:
+    logger.error("Gagal membuat database", exc_info=True)

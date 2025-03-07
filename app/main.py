202246f2d68c67410896705da8eb0db279921e75
MuhammadNurilHuda <nurilhuda3333@gmail.com>
2025-03-07 15:57:14 +0700

    fix logging issues

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from datetime import date
 
-from app.database import SessionLocal, Appointment
+from app.database import SessionLocal, Appointment, UserActivityLog
 from app.chatbot import get_response
 from app.logging import logger
 
@@ -27,14 +27,29 @@ class AppointmentRequest(BaseModel):
     doctor_specialist: str
     appointment_date: date
 
+try:
+    Base.metadata.create_all(bind=engine)
+    logger.info("Tabel database berhasil dibuat atau sudah ada.")
+except Exception as e:
+    logger.error("Gagal membuat tabel database", exc_info=True)
+
 @app.post("/chat")
-def chat(request: ChatRequest):
+def chat(request: ChatRequest, db: Session = Depends(get_db)):
     try:
         logger.info("Menerima request chat.")
-        response = get_response(request.message)
-        logger.debug(f"Response dari chatbot {response}")
-        return {"response": response}
+        response_text = get_response(request.message)
+        logger.debug(f"Response dari chatbot {response_text}")
+
+        user_log = UserActivityLog(prompt=request.message, response=response_text)
+        db.add(user_log)
+        db.commit()
+        db.refresh(user_log)
+
+        logger.info(f"User activity logged dengan id: {user_log.id}")
+
+        return {"response": response_text}
     except Exception as e:
+        logger.error(f"Error pada endpoint /chat", exc_info=True)
         raise HTTPException(status_code=500, detail=str(e))
 
 @app.post("/appointment/book")
@@ -53,6 +68,7 @@ def book_appointment(request: AppointmentRequest, db: Session=Depends(get_db)):
         db.refresh(new_appointment)
         logger.info(f"Appointment berhasil dibuat dengan ID: {new_appointment.id}")
         return {"message":"Appointment booked succesfully", "appointment_id":new_appointment.id}
+    
     except Exception as e:
         logger.error("Error pada endpoint /appointment/book", exc_info=True)
         raise HTTPException(status_code=500, detail=str(e))
